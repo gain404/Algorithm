b90d12192331cc4401bcba87f01762aeb22530fb
gain404 <gain17352@gmail.com>
2025-07-10 20:48:31 +0900

    [level 0] Title: 종이 자르기, Time: 0.20 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.cs
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.cs
@@ -2,8 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        answer = (M - 1) + M*(N- 1);
-        return answer;
+        return (M - 1) + M*(N- 1);
     }
 }
